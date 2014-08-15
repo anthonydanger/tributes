4266806d13f5fe45aee52bfd1f65da9a68d7b314
anthonydanger <anthony.sanchez.sf@gmail.com>
2014-08-15 10:43:09 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Tributes::Application.config.secret_key_base = 'b20ed2f4d40cc9845db66e10b942730f40e0b78e68e0edbf0b31663e71415da12b29320e88fccfdbc7df8c239c4bc5d4ac45419d163dac594c34c13d562ce267'

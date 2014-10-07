7b47183f41b18c708f2771c790ce544f00a7d401
anthonydanger <anthony.sanchez.sf@gmail.com>
2014-10-07 10:52:25 -0700

    Add Amazon S3 for Paperclip to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Tributes::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Sets paperclip to save images to AWS S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
